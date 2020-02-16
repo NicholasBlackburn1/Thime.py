04dbc7e073354c2445e902453b8a21b45c3e4949
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-02-16 07:34:15 -0500

    Revert "Started bot"
    This reverts commit 0e25005d06417fb409e0618e71713f8156a0312f.

--- main.rb
+++
@@ -1,9 +0,0 @@
-require 'discordrb'
-require 'json'
-
-#Discord Robot Config json
-file = File.read('Config.json')
-
-data_hash = JSON.parse(file)
-
-bot = Discordrb::Bot.new token: data_hash['Token']
